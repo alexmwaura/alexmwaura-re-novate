52a5efa0a75b0c7bc9dd459a00157a12a066ffdb
alexmwaura <alexmwaura43@gmail.com>
2020-03-24 04:47:43 +0300

    Updates

---
+++ precache-manifest.a4c2a03e5939188831cba83514189017.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5415d2debab78ba650d",
+    "url": "/re-novate/static/css/main.aebbf290.chunk.css"
+  },
+  {
+    "revision": "d5415d2debab78ba650d",
+    "url": "/re-novate/static/js/main.7949fa0e.chunk.js"
+  },
+  {
+    "revision": "dd763a0114ee4b217006",
+    "url": "/re-novate/static/js/runtime~main.e4925252.js"
+  },
+  {
+    "revision": "a7806323f55ab00817e5",
+    "url": "/re-novate/static/js/2.99ce24c9.chunk.js"
+  },
+  {
+    "revision": "6732bd426c60b47172b1c7296e886a60",
+    "url": "/re-novate/static/media/no-img.6732bd42.png"
+  },
+  {
+    "revision": "8489eec5661c59de64ddda5abe41e8d2",
+    "url": "/re-novate/index.html"
+  }
+];
